781df28f92908c17ee7a2bd7c6ad43e3d732fad3
Vivek Sahoo <89969311+Viveksahoo-svg@users.noreply.github.com>
2025-06-11 22:00:46 +0530

    Time: 16 ms (69.19%), Space: 58.1 MB (43.74%) - LeetHub

---
+++ 0018-4sum/0018-4sum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+        nums.sort((a, b) => a - b);
+    const res = [];
+    for (let i = 0; i < nums.length - 3; i++) {
+        if (i && nums[i] === nums[i - 1]) continue;
+        for (let j = i + 1; j < nums.length - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+            let l = j + 1, r = nums.length - 1;
+            while (l < r) {
+                let sum = nums[i] + nums[j] + nums[l] + nums[r];
+                if (sum === target) {
+                    res.push([nums[i], nums[j], nums[l++], nums[r--]]);
+                    while (l < r && nums[l] === nums[l - 1]) l++;
+                    while (l < r && nums[r] === nums[r + 1]) r--;
+                } else sum < target ? l++ : r--;
+            }
+        }
+    }
+    return res;
+};
