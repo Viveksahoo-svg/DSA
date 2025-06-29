42fcf3dec9aa13beb1ff547dd4c1e76d45298f7f
Vivek Sahoo <89969311+Viveksahoo-svg@users.noreply.github.com>
2025-06-29 21:52:11 +0530

    Time: 7 ms (33.57%), Space: 55.3 MB (90.84%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+        let result = "1";
+    for (let i = 1; i < n; i++) {
+        let temp = "";
+        let count = 1;
+        for (let j = 1; j <= result.length; j++) {
+            if (result[j] === result[j - 1]) {
+                count++;
+            } else {
+                temp += count + result[j - 1];
+                count = 1;
+            }
+        }
+        result = temp;
+    }
+    return result;
+};
